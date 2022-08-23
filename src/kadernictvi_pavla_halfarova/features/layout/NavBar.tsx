cc8e00daae33851e475f2e53e7865d1f40f42da3
Jan Ribka <ribkaj@seznam.cz>
2022-08-23 21:11:49 +0200

    Navbar

--- src/kadernictvi_pavla_halfarova/features/layout/NavBar.tsx
+++ src/kadernictvi_pavla_halfarova/features/layout/NavBar.tsx
@@ -1,17 +1,52 @@
 import { useTranslation } from 'react-i18next';
 
 import { AppBar, Box, Toolbar } from '@mui/material';
+import { useEffect } from 'react';
 
 const NavBar = () => {
   const { t } = useTranslation(["layout\\navBar"]);
 
+  useEffect(() => {
+    window.addEventListener("scroll", HeaderColorChange);
+
+    return () => {
+      window.removeEventListener("scroll", HeaderColorChange);
+    }
+  },[]);
+
+  const HeaderColorChange = () => {
+    const changeColorOnScrollHeight: number = 400;
+    const changeColorOnScrollColor: string = "white";
+    const windowScrollTop = window.pageYOffset;
+debugger;
+    if(windowScrollTop > changeColorOnScrollHeight){
+      // document.body
+      //     .getElementsByTagName("header")[0]          
+      //     .classList.remove(_classes[props.Color]);
+      // document.body
+      //     .getElementsByTagName("header")[0]
+      //     .classList.add(_classes[changeColorOnScrollColor]);
+  }
+  else{
+      // document.body
+      //     .getElementsByTagName("header")[0]
+      //     .classList.add(_classes[props.Color]);
+      // document.body
+      //     .getElementsByTagName("header")[0]
+      //     .classList.remove(_classes[changeColorOnScrollColor]);
+  }
+  }
+
   return (
-    <Box flexGrow={1}>
+    <Box flexGrow={1}>      
       <AppBar>
-        <Toolbar>{t("test")}</Toolbar>
+        <Toolbar style={{color: "w"}}>{t("test")}</Toolbar>
       </AppBar>
     </Box>
   );
 };
 
 export default NavBar;
+
+// Styles
+
