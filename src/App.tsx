cc58acc977964ada4b3fdbb4d30782d9d513d56f
Jan Ribka <ribkaj@seznam.cz>
2022-08-23 20:50:31 +0200

    Cookie consent

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import Layout from 'kadernictvi_pavla_halfarova/features/layout/Layout';
+import CookieConsent from 'react-cookie-consent';
 
 function App() {
-  return <Layout />;
+  return (
+    <>
+      <Layout />
+      <CookieConsent
+        location='bottom'
+        buttonText='ROZUMÍM'
+        cookieName='myAwesomeCookieName2'
+        style={{ background: "rgba(12, 35, 43, 0.8)" }}
+        buttonStyle={{ 
+          color: "#4e503b", 
+          fontSize: "13px",
+          height: "35px",
+          borderRadius: "25px"
+        }}
+        expires={150}
+      >
+        Používáním těchto stránek vyjadřujete souhlas s používáním cookies v
+        souladu s nastavením prohlížeče.
+      </CookieConsent>
+    </>
+  );
 }
 
 export default App;
