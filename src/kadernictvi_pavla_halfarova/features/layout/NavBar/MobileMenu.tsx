f9335ae10d0153648f91b962768e047802632f10
Jan Ribka <jribka@d3soft.cz>
2022-08-29 09:33:12 +0200

    Mobile menu

--- src/kadernictvi_pavla_halfarova/features/layout/NavBar/MobileMenu.tsx
+++ src/kadernictvi_pavla_halfarova/features/layout/NavBar/MobileMenu.tsx
@@ -1,10 +1,13 @@
+import ListItemText from '@mui/material/ListItemText';
 import MenuItem from '@mui/material/MenuItem';
 import MenuList from '@mui/material/MenuList';
 
 const MobileMenu = () => {
   return (
     <MenuList>
-      <MenuItem>Item 1</MenuItem>
+      <MenuItem>
+        <ListItemText>Item 1</ListItemText>
+      </MenuItem>
       <MenuItem>Item 2</MenuItem>
       <MenuItem>Item 3</MenuItem>
       <MenuItem>Item 4</MenuItem>
