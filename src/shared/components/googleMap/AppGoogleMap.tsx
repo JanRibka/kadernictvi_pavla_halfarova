afac141b79dae42893ce017c5dd6f867384bfce6
Jan Ribka <ribkaj@seznam.cz>
2022-08-29 16:36:12 +0200

    AppGoogleMap

--- src/shared/components/googleMap/AppGoogleMap.tsx
+++ src/shared/components/googleMap/AppGoogleMap.tsx
@@ -1,11 +1,19 @@
-import { Map, Marker } from 'google-maps-react';
+// import { Map, Marker } from 'google-maps-react';
+
+import { useJsApiLoader } from '@react-google-maps/api';
 
 const AppGoogleMap = () => {
-    new 
+  // Consts
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "YOUR_API_KEY", // TODO: Api key d8t do souboru
+  });
+
   return (
-    <Map google={}>
-      <Marker ></Marker>
-    </Map>
+    <></>
+    // <Map google={}>
+    //   <Marker ></Marker>
+    // </Map>
   );
 };
 
