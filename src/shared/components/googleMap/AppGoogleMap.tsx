5ad6c462505951023b69204629921d9e02b358a8
Jan Ribka <jribka@d3soft.cz>
2022-08-29 15:02:05 +0200

    AppGoogleMap

---
+++ src/shared/components/googleMap/AppGoogleMap.tsx
@@ -0,0 +1,12 @@
+import { Map, Marker } from 'google-maps-react';
+
+const AppGoogleMap = () => {
+    new 
+  return (
+    <Map google={}>
+      <Marker ></Marker>
+    </Map>
+  );
+};
+
+export default AppGoogleMap;
