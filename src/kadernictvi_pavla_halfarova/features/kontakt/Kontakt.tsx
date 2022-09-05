3e2c058dd98e5a9e462320589110ca68745b1e6f
Jan Ribka <ribkaj@seznam.cz>
2022-09-05 18:52:16 +0200

    Favicon

--- src/kadernictvi_pavla_halfarova/features/kontakt/Kontakt.tsx
+++ src/kadernictvi_pavla_halfarova/features/kontakt/Kontakt.tsx
@@ -18,7 +18,7 @@ const Kontakt = forwardRef(
             <AppGoogleMapsIframe
               title='hair-salon-ph'
               src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d381.36170952929723!2d18.049486077469826!3d49.98350549244384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4711616e6b378b61%3A0xc80b162cab0c5a6d!2zxIxlc2vDoSBzcG_FmWl0ZWxuYSwgYS5zLiAtIEFUTQ!5e0!3m2!1scs!2scz!4v1661885442899!5m2!1scs!2scz'
-              frameBorder=''
+              frameBorder='0'
               style={{ border: "0" }}
               allowFullScreen={false}
               loading='lazy'
