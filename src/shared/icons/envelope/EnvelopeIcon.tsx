0de783ccd867a10cce910a293d9964a50b78eedb
Jan Ribka <ribkaj@seznam.cz>
2022-09-07 17:16:09 +0200

    EnvelopeIcon

--- src/shared/icons/envelope/EnvelopeIcon.tsx
+++ src/shared/icons/envelope/EnvelopeIcon.tsx
@@ -1,8 +1,8 @@
-import Box from "@mui/material/Box";
-import { SxProps, useTheme } from "@mui/material/styles";
+import Box from '@mui/material/Box';
+import { SxProps, useTheme } from '@mui/material/styles';
 
-import CoverLetter from "../../img/cover-letter.png";
-import classes from "./EnvelopeIcon.module.scss";
+import CoverLetter from '../../img/cover-letter.png';
+import classes from './EnvelopeIcon.module.scss';
 
 // https://www.youtube.com/watch?v=4Hm26xcr_dQ
 const EnvelopeIcon = () => {
@@ -11,31 +11,34 @@ const EnvelopeIcon = () => {
 
   // Styles
   const envelopeStyle: SxProps = {
-    width: "300px",
-    height: "200px",
-    position: "relative",
     background: theme.palette.secondary.main,
-    cursor: "pointer",
   };
 
   const topSideStyle: SxProps = {
     borderBottom: "150px solid " + theme.palette.secondary.main,
-    top: "-150px",
-    transformOrigin: "bottom",
-    transform: "rotateX(180deg)",
-    transition: "all 0.8s ease",
-    transitionDelay: "0.6s",
+  };
+
+  const bottomSideStyle: SxProps = {
+    borderBottom: "150px solid " + theme.palette.secondary.dark,
+  };
+
+  const leftSideStyle: SxProps = {
+    borderLeft: "125px solid " + theme.palette.secondary.dark,
+  };
+
+  const rightSideStyle: SxProps = {
+    borderRight: "125px solid " + theme.palette.secondary.dark,
   };
 
   return (
     <Box className={classes.envelope} sx={envelopeStyle}>
       <Box className={classes.topSide} sx={topSideStyle}></Box>
-      <Box className={classes.leftSide}></Box>
+      <Box className={classes.leftSide} sx={leftSideStyle}></Box>
       <Box className={classes.image}>
         <img src={CoverLetter} alt='cover-letter'></img>
       </Box>
-      <Box className={classes.rightSide}></Box>
-      <Box className={classes.bottomSide}></Box>
+      <Box className={classes.rightSide} sx={rightSideStyle}></Box>
+      <Box className={classes.bottomSide} sx={bottomSideStyle}></Box>
     </Box>
   );
 };
