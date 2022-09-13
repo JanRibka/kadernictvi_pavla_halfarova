f5555edad4f87404770605b018e490cfa5a9e573
Jan Ribka <jribka@d3soft.cz>
2022-09-13 12:57:34 +0200

    Section title

--- src/shared/styles/sectionTitle/SectionTitleStyled.tsx
+++ src/shared/styles/sectionTitle/SectionTitleStyled.tsx
@@ -2,13 +2,14 @@ import { styled } from '@mui/material/styles';
 import Typography, { TypographyProps } from '@mui/material/Typography';
 
 const SectionTitleStyled = styled(
-  ({ className, variant, ...props }: TypographyProps) => (
-    <Typography variant='h4' {...props} className={className} />
+  ({ variant, className, ...props }: TypographyProps) => (
+    <Typography variant='h4' className={className} {...props} />
   )
 )`
   margin-bottom: 30px;
   &:before {
     content: "";
+    position: absolute;
     width: 50px;
     height: 2px;
   }
