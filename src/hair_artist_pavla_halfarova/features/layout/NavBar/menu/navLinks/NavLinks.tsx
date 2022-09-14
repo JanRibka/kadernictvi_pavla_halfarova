256f26238a916a3cb75d7e3bf7cd378c52a2b8af
Jan Ribka <ribkaj@seznam.cz>
2022-09-14 20:09:11 +0200

    Menu

--- src/hair_artist_pavla_halfarova/features/layout/NavBar/menu/navLinks/NavLinks.tsx
+++ src/hair_artist_pavla_halfarova/features/layout/NavBar/menu/navLinks/NavLinks.tsx
@@ -1,10 +1,10 @@
-import { forwardRef, MouseEvent, Ref } from "react";
-import { useTranslation } from "react-i18next";
+import { forwardRef, MouseEvent, Ref } from 'react';
+import { useTranslation } from 'react-i18next';
 
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
-import LinkModel from "./LinkModel";
+import LinkModel from './LinkModel';
 
 interface IProps {
   handleOnClick: (event: MouseEvent<HTMLButtonElement> | undefined) => void;
@@ -17,9 +17,10 @@ const NavLinks = forwardRef(
     const { t } = useTranslation(["layout\\mainMenu"]);
     const links: LinkModel[] = [
       { Label: t("intro"), Value: 0 },
-      { Label: t("services"), Value: 1 },
-      { Label: t("gallery"), Value: 2 },
-      { Label: t("contact"), Value: 3 },
+      { Label: t("aboutMe"), Value: 1 },
+      { Label: t("services"), Value: 2 },
+      { Label: t("gallery"), Value: 3 },
+      { Label: t("contact"), Value: 4 },
     ];
 
     return (
