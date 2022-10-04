adb98934ca5ed2bbff93d045fc53bb7e6db7b29f
Jan Ribka <ribkaj@seznam.cz>
2022-10-04 21:24:04 +0200

    Google analytics

--- src/shared/googleAnalytics/SendEventToGA.ts
+++ src/shared/googleAnalytics/SendEventToGA.ts
@@ -1,6 +1,7 @@
 import { event, EventArgs } from 'react-ga';
 
 // TODO: priskrolovani zavolat event pokud je dana sekce aktivni
+// TODO: https://blog.saeloun.com/2022/02/17/how-to-integrate-react-app-with-google-analytics.html
 const SendEventToGA = (
   category: string,
   action: string,
