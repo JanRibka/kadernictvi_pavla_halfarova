b3c6ca5661955a35a297a15d1ebd4a6724f30d15
Jan Ribka <jribka@d3soft.cz>
2022-10-04 23:24:43 +0200

    Cookie consent

---
+++ src/shared/infrastructure/cookieConsent/CookieConsent.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import InitGA from 'shared/googleAnalytics/InitGA';
+import { CookieHelper } from 'shared/helpers/cookieHelper';
+
+import CookieConsentStyled from './CookieConsentStyled';
+
+const CookienConsent = () => {
+  // Consts
+  const cookieHelper: CookieHelper = new CookieHelper();
+  const { t } = useTranslation(["cookieConsent\\cookieConsent"]);
+
+  debugger;
+
+  // Sate
+  // const [viewedCookiePolicy, setViewedCookiePolicy] = useState<string>("");
+
+  // Other
+  useEffect(() => {
+    GetViewedCookiePolicyValue();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const GetViewedCookiePolicyValue = () => {
+    const value: string = cookieHelper.Get("viewed-cookie-policy");
+
+    if (value !== "") {
+      // TODO: Nastavit na wrapper visibilitu, defauktne je display none
+    }
+    // setViewedCookiePolicy(value);
+  };
+
+  const handleAcceptCookie = () => {
+    if (process.env.REACT_APP_GOOGLE_ANALYTICS_ID) {
+      InitGA(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
+    }
+  };
+
+  return (
+    <CookieConsentStyled className='cookie-policy-main-dialog'>
+      {t("mainText")}
+    </CookieConsentStyled>
+
+    // <AppCookieConsent
+    //   location='bottom'
+    //   buttonText={t("buttonText")}
+    //   cookieName='viewed_cookie_policy'
+    //   style={{ background: "rgba(12, 35, 43, 0.8)" }}
+    //   onAccept={handleAcceptCookie}
+    //   buttonStyle={{
+    //     color: "#4e503b",
+    //     fontSize: "13px",
+    //     height: "30px",
+    //     borderRadius: "10px",
+    //   }}
+    //   expires={150}
+    // >
+    //   {t("mainText")}
+    // </AppCookieConsent>
+  );
+};
+
+export default CookienConsent;
