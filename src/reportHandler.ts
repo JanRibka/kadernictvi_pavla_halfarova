a41862d29839cccf8f629e1d57553440b9466456
Jan Ribka <jribka@d3soft.cz>
2022-10-04 14:07:32 +0200

    Web vitals

--- src/reportHandler.ts
+++ src/reportHandler.ts
@@ -1,9 +1,24 @@
+import { ga } from 'react-ga';
 import { Metric } from 'web-vitals';
 
+// event values can only contain integers
+const getEventValueFromMetric = (metric: Metric) => {
+  if (metric.name === "CLS") {
+    return Math.round(metric.value * 1000);
+  }
+  return Math.round(metric.value);
+};
+
 const reportHandler = (metric: Metric) => {
+  debugger;
   //   console.log(metric);
-  const payload = JSON.stringify(metric);
-  navigator.sendBeacon("/analytics", payload);
+  ga("send", "event", {
+    eventCategory: "Web Vitals",
+    eventAction: metric.name,
+    eventValue: getEventValueFromMetric(metric), // values must be integers
+    eventLabel: metric.id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate
+  });
 };
 
 export default reportHandler;
