de94e523746b3b7a1fc0c7cadeab4fe427267ce4
Jan Ribka <jribka@d3soft.cz>
2022-10-10 09:00:28 +0200

    Cookie consent

--- src/shared/infrastructure/cookieConsent/CookieConsent.tsx
+++ src/shared/infrastructure/cookieConsent/CookieConsent.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
-import InitGA from "shared/googleAnalytics/InitGA";
-import { CookieHelper } from "shared/helpers/cookieHelper";
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import InitGA from 'shared/googleAnalytics/InitGA';
+import { CookieHelper } from 'shared/helpers/cookieHelper';
 
-import CookieConsentStyled from "./CookieConsentStyled";
+import CookieConsentStyled from './CookieConsentStyled';
 
-// TODO: Udělat cookie podle dobra energie.cz
+// TODO: Udělat cookie podle dobra energie.cz, nebo https://www.energiezamene.cz/
 const CookienConsent = () => {
   // Consts
   const cookieHelper: CookieHelper = new CookieHelper();
