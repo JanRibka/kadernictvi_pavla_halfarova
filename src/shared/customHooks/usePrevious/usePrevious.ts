72b1e1f24b0980393946841ea1ffaf73cdc570dd
Jan Ribka <jribka@d3soft.cz>
2022-10-10 14:50:50 +0200

    usePrevious

---
+++ src/shared/customHooks/usePrevious/usePrevious.ts
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from 'react';
+
+/**
+ * Hook gets previous value from current value
+ * @param pValue
+ * @returns Previous value
+ */
+const usePrevious = <T>(pValue: T): T | undefined => {
+  // Consts
+  const ref = useRef<T>();
+
+  // Other
+  useEffect(() => {
+    ref.current = pValue as T;
+  });
+
+  return ref.current;
+};
+
+export default usePrevious;
