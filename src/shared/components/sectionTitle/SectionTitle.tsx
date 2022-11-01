c82c88968259c4df2e30c3a33b342a3f4238a1de
Jan Ribka <ribkaj@seznam.cz>
2022-11-01 21:01:59 +0100

    Section title

---
+++ src/shared/components/sectionTitle/SectionTitle.tsx
@@ -0,0 +1,13 @@
+import Typography from '@mui/material/Typography';
+
+import SectionTitleStyled from './styledComponents/SectionTitleStyled';
+
+const SectionTitle = () => {
+  return (
+    <SectionTitleStyled>
+      <Typography></Typography>
+    </SectionTitleStyled>
+  );
+};
+
+export default SectionTitle;
---
+++ src/shared/components/sectionTitle/styledComponents/SectionTitleStyled.ts
@@ -0,0 +1,6 @@
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+
+const SectionTitleStyled = styled(Box)(({ theme }) => ({}));
+
+export default SectionTitleStyled;
