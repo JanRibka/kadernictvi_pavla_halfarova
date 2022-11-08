ce8e427fce1463d57f06766a83532639c4b8aa9a
JanRibka <62721322+JanRibka@users.noreply.github.com>
2022-11-08 17:31:57 +0100

    About me - photo

--- src/kadernictvi_pavla_halfarova/features/kadernictviPH/aboutMe/photo/Photo.tsx
+++ src/kadernictvi_pavla_halfarova/features/kadernictviPH/aboutMe/photo/Photo.tsx
@@ -2,6 +2,7 @@ import pavlaHalfarova from "./img/pavlaHalfarova.jpg";
 import ImageWrapperStyled from "./styledComponents/ImageWrapperStyled";
 
 const Photo = () => {
+  // TODO: nastylovat ramecek tak, ať při hoveru nemusím měnit padding margin
   return (
     <ImageWrapperStyled>
       <img src={pavlaHalfarova} alt='Pavla Halfarová' loading='lazy' />
