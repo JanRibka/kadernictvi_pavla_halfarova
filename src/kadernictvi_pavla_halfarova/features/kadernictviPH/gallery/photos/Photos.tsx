0c16bb6053cd391717694e27ea9389e92d9dd77f
JanRibka <62721322+JanRibka@users.noreply.github.com>
2022-11-27 21:05:10 +0100

    Gallery

--- src/kadernictvi_pavla_halfarova/features/kadernictviPH/gallery/photos/Photos.tsx
+++ src/kadernictvi_pavla_halfarova/features/kadernictviPH/gallery/photos/Photos.tsx
@@ -1,15 +1,22 @@
-import { MouseEvent, useRef, useState } from 'react';
+import Fade from 'kadernictvi_pavla_halfarova/globalStyles/animations/onScroll/fade/Fade';
+import { MouseEvent, useState } from 'react';
 import GridContainer from 'shared/components/gridContainer/GridContainer';
 
+import { useMediaQuery } from '@mui/material';
 import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
 
 import Images from './Images';
 import PhotosDialog from './photosDialog/PhotosDialog';
 import ImgStyled from './styledComponents/ImgStyled';
 import ImgWrapperStyled from './styledComponents/ImgWrapperStyled';
 
-// TODO: Při scrolovani se budou fotky postupně zobrazovat
 const Photos = () => {
+  // Consts
+  const theme = useTheme();
+  const breakPointLgDwn = useMediaQuery(theme.breakpoints.down("lg"));
+  const breakPointSmDwn = useMediaQuery(theme.breakpoints.down("sm"));
+
   // State
   const [openData, setOpenData] = useState<{ open: boolean; index: number }>({
     open: false,
@@ -22,30 +29,45 @@ const Photos = () => {
   };
 
   const RenderPhotos = () => {
-    const refImgWrpper = useRef<HTMLDivElement | null>(null);
+    // Consts
+    let auxIndex = 0;
+    let delay = 0;
+    const picsPerRow = breakPointLgDwn ? 2 : breakPointSmDwn ? 1 : 4;
 
     return Images.map((row, index) => {
       return (
         <GridContainer key={"imagesRowContainer_" + index}>
           {row.map((image, index) => {
+            if (auxIndex !== 0 && auxIndex < picsPerRow) {
+              delay = delay + 300;
+            } else if (auxIndex >= picsPerRow) {
+              delay = 0;
+              auxIndex = 0;
+            }
+
+            auxIndex += 1;
+
             return (
               <ImgWrapperStyled
                 xs={12}
                 sm={6}
                 lg={3}
                 key={"imageWrapper_" + index}
-                ref={refImgWrpper}
               >
-                <ImgStyled
-                  src={image.SrcSection}
-                  alt={image.Alt}
-                  loading='lazy'
-                  onClick={(e) => HandleOnClick(e, image.Index)}
-                />
-                <Box className='line top'></Box>
-                <Box className='line right'></Box>
-                <Box className='line bottom'></Box>
-                <Box className='line left'></Box>
+                <Fade once delay={delay}>
+                  <Box>
+                    <ImgStyled
+                      src={image.SrcSection}
+                      alt={image.Alt}
+                      loading='lazy'
+                      onClick={(e) => HandleOnClick(e, image.Index)}
+                    />
+                    <Box className='line top'></Box>
+                    <Box className='line right'></Box>
+                    <Box className='line bottom'></Box>
+                    <Box className='line left'></Box>
+                  </Box>
+                </Fade>
               </ImgWrapperStyled>
             );
           })}
