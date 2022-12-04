ec0066271beb53bb7a597cfe130894a1a198af05
JanRibka <62721322+JanRibka@users.noreply.github.com>
2022-12-04 19:50:18 +0100

    contact

--- src/kadernictvi_pavla_halfarova/features/kadernictviPH/contact/contacts/Contacts.tsx
+++ src/kadernictvi_pavla_halfarova/features/kadernictviPH/contact/contacts/Contacts.tsx
@@ -1,10 +1,10 @@
-import { ContactTypeEnum } from 'shared/enums/ContactTypeEnum';
+import { ContactTypeEnum } from "shared/enums/ContactTypeEnum";
 
-import Box from '@mui/material/Box';
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import Box from "@mui/material/Box";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
-import Contact from './contact/Contact';
+import Contact from "./contact/Contact";
 
 const Contacts = () => {
   // Consts
@@ -28,8 +28,8 @@ const Contacts = () => {
       {/* Address */}
       <Contact
         contactType={ContactTypeEnum.Address}
-        contact='Slezská 104/57, 747 27 Kobeřice'
-        addressMapLink='https://www.google.com/maps/place/Slezsk%C3%A1+104%2F57,+747+27+Kobe%C5%99ice/@49.9837184,18.0495592,19.25z/data=!4m5!3m4!1s0x4711616e6b5b1b55:0xcbf9c14250f2684c!8m2!3d49.9837204!4d18.0500168?hl=cs'
+        contact='Slezská 57, 747 27 Kobeřice'
+        addressMapLink='https://goo.gl/maps/7HxtznJivoBxDWYh6'
       />
     </Box>
   );
--- src/kadernictvi_pavla_halfarova/features/kadernictviPH/contact/googleMaps/GoogleMaps.tsx
+++ src/kadernictvi_pavla_halfarova/features/kadernictviPH/contact/googleMaps/GoogleMaps.tsx
@@ -1,8 +1,8 @@
 const GoogleMaps = () => {
   return (
     <iframe
-      title='Kadeřnictví Pavla Halfarová - Mapa'
-      src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d381.36170952929723!2d18.049486077469826!3d49.98350549244384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4711616e6b378b61%3A0xc80b162cab0c5a6d!2zxIxlc2vDoSBzcG_FmWl0ZWxuYSwgYS5zLiAtIEFUTQ!5e0!3m2!1scs!2scz!4v1661885442899!5m2!1scs!2scz'
+      title='Hair Salon Pavla Halfarová - Mapa'
+      src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1814.1030362845204!2d18.04880593054422!3d49.98268866785533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471161a0b0ab0411%3A0x6ca8d0574862b657!2sHairsalon%20PH!5e0!3m2!1scs!2scz!4v1670179135015!5m2!1scs!2scz" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade'
       width='100%'
       height='100%'
       frameBorder=''
