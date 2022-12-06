a686dc7ee65c28ef826f6d26a921bff2c50aee2e
JanRibka <62721322+JanRibka@users.noreply.github.com>
2022-12-06 22:23:18 +0100

    Přidání loga do suspense fallback

--- src/shared/components/suspenseFallback/SuspenseFallback.tsx
+++ src/shared/components/suspenseFallback/SuspenseFallback.tsx
@@ -1,11 +1,11 @@
-import AppLoader from '../loader/AppLoader';
-import SuspenseFallbackStyled from './styledComponents/SuspenseFallbackStyled';
+import HairSalonPHLogo from "../../img/logo/logo_hairsalon_ph.jpg";
+import SuspenseFallbackStyled from "./styledComponents/SuspenseFallbackStyled";
 
 const SuspenseFallback = () => {
   //TODO: Dát tady logo
   return (
     <SuspenseFallbackStyled>
-      <AppLoader />
+      {<img src={HairSalonPHLogo} alt='Hair Salon PH - Logo'></img>}
     </SuspenseFallbackStyled>
   );
 };
