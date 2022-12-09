7791c16513b43c81d20039cf8b7011a14768d279
JanRibka <62721322+JanRibka@users.noreply.github.com>
2022-12-09 15:00:50 +0100

    GoogleAnalzticsHelper

--- src/shared/helpers/googleAnalyticsHelper.ts
+++ src/shared/helpers/googleAnalyticsHelper.ts
@@ -10,6 +10,7 @@ export class GoogleAnalyticsHelper {
   }
 
   // TODO: priskrolovani zavolat event pokud je dana sekce aktivni
+  // TODO: Bude zde metoda, která bude načátat hodnoty z cookies a vratí hodnotu pro analztické cookie
   // TODO: https://blog.saeloun.com/2022/02/17/how-to-integrate-react-app-with-google-analytics.html
   SendEventToGA = (category: string, action: string, label: string = "") => {
     const value: string = cookieHelper.Get("CookieConsent");
