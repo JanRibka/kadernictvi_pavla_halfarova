785fc5288ff313d341d2b79f9dc4e808dd1cda32
JanRibka <62721322+JanRibka@users.noreply.github.com>
2022-12-13 19:41:15 +0100

    Odstranění ssr

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,20 @@
 // LOCALIZATION
-import "./shared/infrastructure/localize/i18n";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import "@fontsource/caveat";
+import './shared/infrastructure/localize/i18n';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/caveat';
 
-import { AppTheme } from "kadernictvi_pavla_halfarova/globalStyles/AppTheme";
-import React, { Suspense } from "react";
-import ReactDOM from "react-dom/client";
-import { useSSR } from "react-i18next";
-import SuspenseFallback from "shared/components/suspenseFallback/SuspenseFallback";
+import { AppTheme } from 'kadernictvi_pavla_halfarova/globalStyles/AppTheme';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import { CssBaseline } from "@mui/material";
-import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import { CssBaseline } from '@mui/material';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
 
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-// const AppContainer = () => {
-//   useSSR(window.initialI18nStore, window.initialLanguage);
-//   return (
-//     <React.StrictMode>
-//       <ThemeProvider theme={AppTheme}>
-//         <CssBaseline />
-//         <Suspense fallback={<SuspenseFallback />}>
-//           <App />
-//         </Suspense>
-//       </ThemeProvider>
-//     </React.StrictMode>
-//   );
-// };
-
-// ReactDOM.hydrateRoot(
-//   document.getElementById("root") as HTMLElement,
-//   AppContainer()
-// );
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -47,9 +26,7 @@ root.render(
   <React.StrictMode>
     <ThemeProvider theme={AppTheme}>
       <CssBaseline />
-      {/* <Suspense fallback={<SuspenseFallback />}> */}
       <App />
-      {/* </Suspense> */}
     </ThemeProvider>
   </React.StrictMode>
 );
