226282f603012da82b126cbf14346bef7d91f313
JanRibka <62721322+JanRibka@users.noreply.github.com>
2022-12-17 01:16:25 +0100

    Úprava stylů na logu

--- src/kadernictvi_pavla_halfarova/features/layout/NavBar/logo/styledComponents/ImgWrapperStyled.ts
+++ src/kadernictvi_pavla_halfarova/features/layout/NavBar/logo/styledComponents/ImgWrapperStyled.ts
@@ -1,17 +1,18 @@
-import IconButton from "@mui/material/IconButton";
-import { styled } from "@mui/material/styles";
+import IconButton from '@mui/material/IconButton';
+import { styled } from '@mui/material/styles';
 
 const ImgWrapperStyled = styled(IconButton)(({ theme }) => ({
   padding: "0",
   width: "320px",
   height: "320px",
-  borderRadius: "0",
+  borderRadius: "50%",
   transition: `width 300ms ease-out,
   height 500ms ease-out`,
 
   img: {
     height: "inherit",
     borderRadius: "inherit",
+    backgroundColor: theme.palette.secondary.main,
   },
 }));
 
