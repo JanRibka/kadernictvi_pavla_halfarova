523998b6ca2b8dc424036028e1617b6c48713259
JanRibka <62721322+JanRibka@users.noreply.github.com>
2022-12-18 10:05:03 +0100

    Fade - drbky

--- src/kadernictvi_pavla_halfarova/globalStyles/animations/onScroll/fade/Fade.tsx
+++ src/kadernictvi_pavla_halfarova/globalStyles/animations/onScroll/fade/Fade.tsx
@@ -3,6 +3,7 @@ import { useRef } from 'react';
 import Box from '@mui/material/Box';
 
 interface IProps {
+  /** Animation name */
   animation:
     | "fadeIn"
     | "fadeInDown"
@@ -17,9 +18,13 @@ interface IProps {
     | "fadeInTopRight"
     | "fadeInBottomLeft"
     | "fadeInBottomRight";
+  /** Animation duration (ms) */
   duration?: number;
+  /** Delay before the animation starts (ms) */
   delay?: number;
+  /** Distance to start the animation (related to the browser bottom) (px) */
   offset?: number;
+  /** Number of times the animation is repeated */
   iteration?: number;
   children: JSX.Element;
 }
@@ -33,8 +38,8 @@ const Fade: React.FC<IProps> = (props) => {
       ref={refWrapper}
       sx={{ lineHeight: 0 }}
       data-wow-offset={props.offset?.toString()}
-      data-wow-duration={props.duration?.toString() + "s"}
-      data-wow-delay={props.delay?.toString() + "s"}
+      data-wow-duration={((props.duration as number) / 1000)?.toString() + "s"}
+      data-wow-delay={((props.delay as number) / 1000)?.toString() + "s"}
       data-wow-iteration={props.iteration?.toString()}
       className={
         "wow animate__animated animate__" +
@@ -48,7 +53,7 @@ const Fade: React.FC<IProps> = (props) => {
 };
 
 Fade.defaultProps = {
-  duration: 2,
+  duration: 2000,
   delay: 0,
   offset: 0,
   iteration: 1,
