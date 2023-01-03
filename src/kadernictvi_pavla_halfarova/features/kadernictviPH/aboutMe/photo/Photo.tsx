f68b8fbb1f653f4c61de477498785eb8aec4c5b2
JanRibka <62721322+JanRibka@users.noreply.github.com>
2023-01-03 18:25:05 +0100

    About me- Zakomentování swiperu

--- src/kadernictvi_pavla_halfarova/features/kadernictviPH/aboutMe/photo/Photo.tsx
+++ src/kadernictvi_pavla_halfarova/features/kadernictviPH/aboutMe/photo/Photo.tsx
@@ -3,53 +3,40 @@ import 'swiper/css';
 import 'swiper/css/lazy';
 
 // import required modules
-import { Autoplay, Lazy } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
+// import { Autoplay, Lazy } from 'swiper';
+// import { Swiper, SwiperSlide } from 'swiper/react';
 import pavlaHalfarova from './img/pavlaHalfarova.jpg';
 import ImageWrapperStyled from './styledComponents/ImageWrapperStyled';
 
 const Photo = () => {
   return (
     <ImageWrapperStyled>
-      <Swiper lazy autoplay loop modules={[Autoplay, Lazy]}>
+      {/* <Swiper lazy autoplay loop modules={[Autoplay, Lazy]}>
+        <SwiperSlide> */}
+      <img src={pavlaHalfarova} alt='Pavla Halfarová' loading='lazy' />
+      {/* </SwiperSlide>
         <SwiperSlide>
           <img
             src={pavlaHalfarova}
             alt='Pavla Halfarová'
-            loading='lazy'
-            // className='swiper-lazy'
-          />
-          {/* <div className='swiper-lazy-preloader swiper-lazy-preloader-white'></div> */}
+            loading='lazy'            
+          />          
         </SwiperSlide>
         <SwiperSlide>
           <img
             src={pavlaHalfarova}
             alt='Pavla Halfarová'
-            loading='lazy'
-            // loading='lazy'
-          />
-          {/* <div className='swiper-lazy-preloader swiper-lazy-preloader-white'></div> */}
+            loading='lazy'            
+          />          
         </SwiperSlide>
         <SwiperSlide>
           <img
             src={pavlaHalfarova}
             alt='Pavla Halfarová'
-            loading='lazy'
-            // className='swiper-lazy'
-          />
-          {/* <div className='swiper-lazy-preloader swiper-lazy-preloader-white'></div> */}
+            loading='lazy'            
+          />          
         </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src={pavlaHalfarova}
-            alt='Pavla Halfarová'
-            loading='lazy'
-            // className='swiper-lazy'
-          />
-          {/* <div className='swiper-lazy-preloader swiper-lazy-preloader-white'></div> */}
-        </SwiperSlide>
-      </Swiper>
+      </Swiper> */}
     </ImageWrapperStyled>
   );
 };
